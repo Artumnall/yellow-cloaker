67285bb6c242ace3d652de64b29055f2f554602a
Artumnall <arthurtonicocriador@gmail.com>
2025-05-01 14:38:07 -0300

    Create iputils.php

---
+++ yellow-cloaker/bases/iputils.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Sinergi\BrowserDetector;
+
+class IpUtils
+{
+    public static function getUserIp(): string
+    {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            return $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            return explode(',', $_SERVER['HTTP_X_FORWARDED_FOR'])[0];
+        } else {
+            return $_SERVER['REMOTE_ADDR'] ?? 'UNKNOWN';
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
